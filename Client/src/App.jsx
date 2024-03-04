c07aed144e0f0ab256e06b2c476a5b16129e1fb9
Farhathcader <farhadfd818@mail.com>
2024-03-04 12:04:43 +0530

    routes added

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,26 +1,23 @@
 import "./App.css";
-//import Contact from "./Components/Contact";
+import Contact from "./Components/Contact";
 import Home from "./Pages/Home";
 //import Footer from "./Components/Footer";
 //import Front from "./Components/Front";
 //import Login from "./Components/Login";
 //import Register from "./Components/Register";
-//import Signup from "./Components/Signup";
-//import Navbar from "./Components/Navbar";
+import Signup from "./Components/Signup";
+import {BrowserRouter ,Routes, Route} from "react-router-dom";
 
 function App() {
   return (
-    <>
-            {/* <Navbar />
-      <div className="bg-indigo-600 w-full h-screen"></div> 
-      <Login /> 
-      <Register/> 
-      <Signup/> 
-      <Front/> 
-      <Footer/> 
-      <Contact/> */}
-      <Home/>
-    </>
+    
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/contactus" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
