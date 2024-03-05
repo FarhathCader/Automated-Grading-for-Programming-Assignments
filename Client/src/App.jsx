2d7be3697474afbf89c573734ade1b843a8d3287
mhdmusharraf <mhdmusharraf.edu@gmail.com>
2024-03-05 09:20:06 +0530

    Signup added

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,12 +1,15 @@
 import "./App.css";
 import Contact from "./Components/Contact";
 import Home from "./Pages/Home";
-//import Footer from "./Components/Footer";
-//import Front from "./Components/Front";
-//import Login from "./Components/Login";
-//import Register from "./Components/Register";
+import Footer from "./Components/Footer";
+import Front from "./Components/Front";
+import Login from "./Components/Login";
+import Register from "./Components/Register";
 import Signup from "./Components/Signup";
 import {BrowserRouter ,Routes, Route} from "react-router-dom";
+import FrontSignup from "./Components/FrontSignup";
+import MainSignup from "./Pages/MainSignup";
+import ContactUs from "./Pages/ContactUs";
 
 function App() {
   return (
@@ -14,10 +17,12 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/contactus" element={<Contact />} />
+        <Route path="/signup" element={<MainSignup />} />
+        <Route path="/contactus" element={<ContactUs />} />
       </Routes>
     </BrowserRouter>
+    
+   
   );
 }
 
--- Client/src/Components/Contact.jsx
+++ Client/src/Components/Contact.jsx
@@ -22,9 +22,9 @@ const Contact = () => {
       };
     
   return (
-    <div className="bg-gradient-to-r from-[#F28383] from-10% via-[#9D6CD2] to-[#481EDC] to-90% min-h-screen flex items-center justify-center">
-    <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 w-full max-w-md" onSubmit={handleSubmit}>
-      <h2 className="text-2xl font-bold text-gray-800 mb-4">Contact Us</h2>
+    <div className="bg-gray-200 w-full min-h-screen flex items-center justify-center">
+    <form className="bg-white shadow-md rounded px-8 pt-2 pb-8 mb-4 mt-12 w-full max-w-md" onSubmit={handleSubmit}>
+      <h2 className="text-2xl font-bold text-violet-700 mb-4">Contact Us</h2>
       <div className="mb-4">
         <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="name">
           Name
