0c5de8bcdc504d482ce3db7087ce4b4d20b47d50
mhdmusharraf <mhdmusharraf.edu@gmail.com>
2024-03-11 15:59:28 +0530

    Admin dashboard Created

--- Client/src/Pages/StudentDashboard.jsx
+++ Client/src/Pages/StudentDashboard.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+//import { Link } from 'react-router-dom'
 import Sidebar from '../Sections/Sidebar'
 import Feed from '../Sections/Feed'
 
 export default function StudentDashboard() {
+  console.log("hello");
   return (
+   
     <main className='w-full h-screen flex justify-between items-start'>
       <Sidebar/>
       <Feed/>
