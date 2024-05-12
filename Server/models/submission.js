7139c6ea5ed2821533adb53c9c67f2cc06081baf
Farhathcader <farhadfd818@mail.com>
2024-05-12 12:45:03 +0530

    unit test added

--- Server/models/submission.js
+++ Server/models/submission.js
@@ -21,6 +21,7 @@ const submissionSchema = new mongoose.Schema({
     userId : {
         type : mongoose.Schema.Types.ObjectId,
         ref : 'User',
+        required : true
     },
     // status: {
     //     type: String,
