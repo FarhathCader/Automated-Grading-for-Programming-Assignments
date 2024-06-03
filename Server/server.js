d99d320ed751c30a13ff57e4607988a1325284db
Farhathcader <farhadfd818@mail.com>
2024-06-03 14:08:04 +0530

    heroku build

--- Server/server.js
+++ Server/server.js
@@ -21,11 +21,10 @@ mongoose
 
 
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('../client/dist'));
+    app.use(express.static(path.join(__dirname, '../client/dist')));
     app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'));
-    }
-    );
-  }
+        res.sendFile(path.resolve(__dirname, '../client/dist', 'index.html'));
+    });
+}
 
   module.exports = app;
