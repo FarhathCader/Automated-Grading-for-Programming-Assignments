0a835421f6462f19b5dd55a38606981a15bc60a0
Farhathcader <farhadfd818@mail.com>
2024-06-03 14:43:17 +0530

    heroku build

--- Server/app.js
+++ Server/app.js
@@ -7,7 +7,7 @@ app.use(cookieParser());
 // app.use(express.json());
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb', extended: true, parameterLimit: 50000}));
-app.use(cors({ credentials: true, origin: "https://zee-code-3234074b267f.herokuapp.com/" }));
+app.use(cors({ credentials: true, origin: "https://zee-code-3234074b267f.herokuapp.com" }));
 app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
