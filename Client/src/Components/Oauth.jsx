58d73d6006657dddefbbd5e88f5938877de43471
mhdmusharraf <mhdmusharraf.edu@gmail.com>
2024-06-26 02:57:13 +0530

    Google login ui done

--- Client/src/Components/Oauth.jsx
+++ Client/src/Components/Oauth.jsx
@@ -1,37 +1,43 @@
-import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-import { GoogleAuthProvider, getAuth, signInWithPopup } from 'firebase/auth'
-import { app } from '../firebase';
+// import { GoogleAuthProvider, getAuth, signInWithPopup } from 'firebase/auth'
+import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+
+import { app } from "../firebase";
+import { FaGoogle } from "react-icons/fa";
 
 export default function Oauth(props) {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
-
-    const handleGoogleClick = async ()=>{
-      const provider = new GoogleAuthProvider();
-      provider.setCustomParameters({
-        prompt: 'select_account'
+  const handleGoogleClick = async () => {
+    const provider = new GoogleAuthProvider();
+    provider.setCustomParameters({
+      prompt: "select_account",
+    });
+    try {
+      const resultFromGoogle = await signInWithPopup(getAuth(app), provider);
+      console.log(resultFromGoogle.user);
+      navigate("/googleauth", {
+        state: {
+          username: resultFromGoogle.user.displayName,
+          email: resultFromGoogle.user.email,
+          usertype: props.usertype,
+        },
       });
-      try{
-
-        const resultFromGoogle = await signInWithPopup(getAuth(app), provider)
-        console.log(resultFromGoogle.user);
-        navigate('/googleauth',{state : {username : resultFromGoogle.user.displayName , email : resultFromGoogle.user.email, usertype: props.usertype}})
-
-      }catch(err){
-        console.log(err)
-      }
+    } catch (err) {
+      console.log(err);
     }
-
+  };
 
   return (
     <div>
-
-        <button onClick={handleGoogleClick}>
-            Sign in with Google
-        </button>
-      
+      <button
+        onClick={handleGoogleClick}
+        className="flex items-center justify-center w-80 bg-red-600 text-white font-semibold rounded-full py-2 px-4 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2"
+      >
+        <FaGoogle className="mr-2" /> Sign in with Google
+      </button>
     </div>
-  )
+  );
 }
