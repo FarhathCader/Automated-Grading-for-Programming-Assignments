a3c42cea7e97714c123a01adba014f0e16a10fb6
mhdmusharraf <mhdmusharraf.edu@gmail.com>
2024-06-27 15:47:15 +0530

    Minor changes in the dasboard

--- Client/src/Pages/Lecturer/LecturerDashBoard.jsx
+++ Client/src/Pages/Lecturer/LecturerDashBoard.jsx
@@ -5,8 +5,8 @@ import SidebarLecturer from "../../Sections/SidebarLecturer";
 import Header from "../../Sections/Header";
 import axios from "axios";
 import ClipLoader from "react-spinners/ClipLoader";
-import {  CSSProperties } from "react";
-import { ToastContainer,toast } from 'react-toastify';
+import { CSSProperties } from "react";
+import { ToastContainer, toast } from "react-toastify";
 import { authActions } from "../../store";
 import { backendUrl } from "../../../config";
 import Approval from "./Approval";
@@ -23,12 +23,14 @@ const LecturerDashBoard = () => {
   const [isApproved, setIsApproved] = useState(true);
   const [lecturer, setLecturer] = useState(null);
   const [loading, setLoading] = useState(true);
-  const user  = useSelector(state => state.user);
+  const user = useSelector((state) => state.user);
   const dispatch = useDispatch();
 
   const handleLogout = async () => {
     try {
-      const response = await axios.post(`${backendUrl}/api/user/logout`, null, { withCredentials: true });
+      const response = await axios.post(`${backendUrl}/api/user/logout`, null, {
+        withCredentials: true,
+      });
       const data = response.data;
       if (response.status === 200) {
         toast.success(data.msg);
@@ -38,8 +40,7 @@ const LecturerDashBoard = () => {
       }
     } catch (error) {
       toast.error("An error occurred during logout.");
-      dispatch(authActions.logout())
-
+      dispatch(authActions.logout());
     }
   };
 
@@ -48,12 +49,11 @@ const LecturerDashBoard = () => {
   }, [user]);
 
   const fetchLecturer = async () => {
-
     setLoading(true);
     try {
-
-      if(user._id === undefined) return;
-      const res = user && await axios.get(`${backendUrl}/api/lecturer/${user._id}`);
+      if (user._id === undefined) return;
+      const res =
+        user && (await axios.get(`${backendUrl}/api/lecturer/${user._id}`));
       const data = res && (await res.data);
       if (data) {
         setLecturer(data);
@@ -61,69 +61,50 @@ const LecturerDashBoard = () => {
       }
     } catch (err) {
       toast.error("error", err.message);
-    }finally{
+    } finally {
       setLoading(false);
     }
   };
 
-
-
   return (
     <main className="w-full h-screen flex justify-between items-start text-xs md:text-base">
-     {/* {isApproved ?  <SidebarLecturer  /> : null} */}
-     { 
-     !loading ? 
-    ( <section className="w-4/5 grow bg-white h-screen overflow-y-auto flex flex-col justify-start items-center gap-4 p-4">
-        {/* <Header bgColor="fuchsia" /> */}
-       
-      
-        <div className="w-100 p-6 bg-fuchsia-100 rounded-xl shadow-lg flex flex-col items-center mt-20">
-          <h1 className="text-xl md:text-2xl font-bold text-fuchsia-700 mb-4">
-            Welcome to your Dashboard!
-          </h1>
-         
-         
-        <div className="mb-6 flex flex-col items-center justify-center">
-            <p className="text-gray-600 text-sm md:text-base mb-4">
-              Explore the available contests or start practicing to sharpen your skills.
-            </p>
-            <div className="flex justify-center items-center gap-4">
-              <Link to="/qbank">
-                <button className="bg-fuchsia-500 text-white px-6 py-2 rounded-md hover:bg-fuchsia-600 focus:outline-none">
-                  Questions Bank
-                </button>
-              </Link>
-              <Link to="/contest">
-                <button className="bg-green-500 text-white px-6 py-2 rounded-md hover:bg-green-600 focus:outline-none">
-                  Contests
-                </button>
-              </Link>
+      {/* {isApproved ?  <SidebarLecturer  /> : null} */}
+      {!loading ? (
+        <section className="w-4/5 grow bg-white h-screen overflow-y-auto flex flex-col justify-start items-center gap-4 p-4">
+          {/* <Header bgColor="fuchsia" /> */}
+
+          <div className="w-100 p-6 bg-fuchsia-100 rounded-xl shadow-lg flex flex-col items-center mt-20">
+            <h1 className="text-xl md:text-2xl font-bold text-fuchsia-700 mb-4">
+              Welcome to your Dashboard!
+            </h1>
+
+            <div className="mb-6 flex flex-col items-center justify-center">
+              <p className="text-gray-600 text-sm md:text-base mb-4">
+                Manage and create contests to challenge your students and foster
+                their skills.
+              </p>
+              <div className="flex justify-center items-center gap-4">
+                <Link to="/qbank">
+                  <button className="bg-fuchsia-500 text-white px-6 py-2 rounded-md hover:bg-fuchsia-600 focus:outline-none">
+                    Questions Bank
+                  </button>
+                </Link>
+                <Link to="/contest">
+                  <button className="bg-green-500 text-white px-6 py-2 rounded-md hover:bg-green-600 focus:outline-none">
+                    Contests
+                  </button>
+                </Link>
+              </div>
             </div>
+
+            {!isApproved && <Approval handleLogout={handleLogout} />}
           </div>
-
-         {!isApproved && <Approval handleLogout={handleLogout} />}
-          
-        
-
-          
-        </div>
-        
-      
-      </section>)
-      :
-      (
-      
+        </section>
+      ) : (
         <div className="w-full flex justify-center items-center h-screen">
-              <ClipLoader
-                color="red"
-                loading={true}
-                size={150}
-                css={override}
-              />
-            </div>
-      
-      )
-    }
+          <ClipLoader color="red" loading={true} size={150} css={override} />
+        </div>
+      )}
     </main>
   );
 };
